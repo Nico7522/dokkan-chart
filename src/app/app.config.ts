8c0282155cac44381c086f0703279d7c0b78a6e4
Nicolas <nico.daddabbo7100@gmail.com>
2026-03-21 12:39:49 +0100

    add vercel analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { inject } from '@vercel/analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideCharts(withDefaultRegisterables()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'production' });
+      },
+    },
   ],
 };
